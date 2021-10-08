c25a32b4f66dc23b5c9a4b6bfbac3fa06af71b65
udeme <udeme.jalekun@venturegardengroup.com>
2021-10-08 11:58:34 +0100

    search for cypress

--- cypress/integration/test.spec.js
+++ cypress/integration/test.spec.js
@@ -3,7 +3,8 @@
 describe ('Google search page', function () {
     it ('Launch google search page', function() {
         cy.visit('https://www.google.com/', { retryOnNetworkFailure: true });
-        cy.wait(3000)
-        cy.get('.lnXdpd').should('be.visible')
+        cy.wait(3000);
+        cy.get('.lnXdpd').should('be.visible');
+        cy.get('.gLFyf').type('cypress.io{enter}');
     })
 })
